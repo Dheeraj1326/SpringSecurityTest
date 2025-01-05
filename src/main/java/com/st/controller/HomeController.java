74fb74b6fbb2514b4c90935984eda9892da8707b
Dheeraj1326 <dheeraj2698dm@gmail.com>
2025-01-05 19:41:49 +0530

    Update Changes for project

---
+++ src/main/java/com/st/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.st.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+	@GetMapping("/")
+	private String home() {
+		return "Home ";
+	}
+}
